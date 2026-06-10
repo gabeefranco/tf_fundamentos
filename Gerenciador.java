2591107062d002be0f132ae693c259d893739505
Gabriel Franco <gabefgonc@gmail.com>
2026-06-10 20:31:20 -0300

    classe gerenciador e dados com métodos placeholder

---
+++ Gerenciador.java
@@ -0,0 +1,63 @@
+public class Gerenciador {
+  private Dados dados;
+
+  public Gerenciador(Dados dados) {
+    this.dados = dados;
+  }
+
+  public void cadastrarAluno() {
+
+  }
+
+  public void cadastrarBolsista() {
+
+  }
+
+  public void registrarAcompanhamento() {
+  }
+
+  public void listarAlunos() {
+
+  }
+
+  public void listarBolsistas() {
+  }
+
+  public void maiorNome() {
+
+  }
+
+  public void vogaisNomes() {
+
+  }
+
+  public void percentualAlunos() {
+
+  }
+
+  public void mediaIdadeAlunos() {
+  }
+
+  public void criarChamada() {
+
+  }
+
+  public void chamada() {
+
+  }
+
+  public void calcularRisco() {
+  }
+
+  public void relatorioRisco() {
+
+  }
+
+  public void altoRisco() {
+
+  }
+
+  public void inovacao() {
+
+  }
+}
